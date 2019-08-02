427528fee5b7fe25ee9de1f8a08672a874c0ef29
47913 <479138040@qq.com>
2019-08-02 15:06:19 +0800

    仓库创建

---
+++ off_package.bat
@@ -0,0 +1,3 @@
+cd E:\idea_project\target
+jar cvf .\classes\com\cyy\day\controller\TestDockerController.class .\day-0.0.1-SNAPSHOT.war
+pause
---
+++ src/main/java/com/cyy/day/controller/TestDockerController.java
@@ -0,0 +1,18 @@
+package com.cyy.day.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class TestDockerController {
+   private static final Logger logger = LoggerFactory.getLogger(TestDockerController.class);
+    @RequestMapping(value = "/",method={RequestMethod.GET})
+    public String test() {
+        logger.info("调用成功2!");
+        return "test";
+    }
+}
