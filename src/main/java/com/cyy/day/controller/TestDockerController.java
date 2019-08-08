8d495cb7aaaf6245e273724cdfad08f5cc36c5c3
47913 <479138040@qq.com>
2019-08-08 09:58:15 +0800

    测试

--- src/main/java/com/cyy/day/controller/TestDockerController.java
+++ src/main/java/com/cyy/day/controller/TestDockerController.java
@@ -3,16 +3,20 @@ package com.cyy.day.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@CrossOrigin
+@RestController
+@RequestMapping("/TestDocker")
 public class TestDockerController {
    private static final Logger logger = LoggerFactory.getLogger(TestDockerController.class);
-    @RequestMapping(value = "/",method={RequestMethod.GET})
-    public String test() {
-        logger.info("调用成功2!");
-        return "test";
+    @RequestMapping(value = "/success",method={RequestMethod.GET})
+    public Object success() {
+        Map map = new HashMap();
+        map.put("success", "获取数据成功！");
+        return map;
     }
 }
