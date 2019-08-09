6e0d9128197e031d30ee7f2fa9ed17d2cd95d26d
47913 <479138040@qq.com>
2019-08-09 15:49:24 +0800

    加入自定义mvc配置

--- src/main/java/com/cyy/day/config/MyWebMvcConfig.java
+++ src/main/java/com/cyy/day/config/MyWebMvcConfig.java
@@ -1,17 +1,29 @@
 package com.cyy.day.config;
 
+import com.cyy.day.util.SpringBeanUtil;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*
 加入支持全局跨域
  */
+@Configuration
 public class MyWebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
+        registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS")
                 .allowedOrigins("http://localhost:8089");
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration loginRegistry = registry.addInterceptor(SpringBeanUtil.getBean(LoginInterceptor.class));
+        loginRegistry.addPathPatterns("/**");
+    }
+
 }
