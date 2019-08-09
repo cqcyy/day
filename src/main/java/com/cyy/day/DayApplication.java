21bcfb7cded693ab5182e8f417cf1b10d7755801
47913 <479138040@qq.com>
2019-08-09 15:05:48 +0800

    测试

--- src/main/java/com/cyy/day/DayApplication.java
+++ src/main/java/com/cyy/day/DayApplication.java
@@ -2,9 +2,6 @@ package com.cyy.day;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class DayApplication {
@@ -12,11 +9,5 @@ public class DayApplication {
     public static void main(String[] args) {
         SpringApplication.run(DayApplication.class, args);
     }
-    @Bean
-    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
-        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
-        registration.getUrlMappings().clear();
-        registration.addUrlMappings("/*");
-        return registration;
-    }
+
 }
